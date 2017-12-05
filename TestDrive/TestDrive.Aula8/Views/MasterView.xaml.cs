cfa39806e39d0dce115547c99290f94264033448
Gabriel Carolino Terceiro <gcarolinoterceiro@gmail.com>
2017-12-05 09:10:32 -0200

    gjdkfn

--- TestDrive/TestDrive.Aula8/Views/MasterView.xaml.cs
+++ TestDrive/TestDrive.Aula8/Views/MasterView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +13,11 @@ using Xamarin.Forms.Xaml;
 namespace TestDrive.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MasterDetailViewDetail : ContentPage
+    public partial class MasterView : ContentPage
     {
-        public MasterDetailViewDetail()
+        public ListView ListView;
+
+        public MasterView()
         {
             InitializeComponent();
         }
