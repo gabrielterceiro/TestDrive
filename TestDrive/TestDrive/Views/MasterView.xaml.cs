99cca492af625476a2c949d3d4c0c6f7b98b7629
Gabriel Carolino Terceiro <gcarolinoterceiro@gmail.com>
2017-12-06 15:36:28 -0200

    Aula III - MasterView: Criação de uma TabbedPage no MasterDetailView

--- TestDrive/TestDrive/Views/MasterView.xaml.cs
+++ TestDrive/TestDrive/Views/MasterView.xaml.cs
@@ -12,16 +12,12 @@ using Xamarin.Forms.Xaml;
 
 namespace TestDrive.Views
 {
-    public partial class MasterView : ContentPage
+    public partial class MasterView : TabbedPage
     {
-        public MasterViewModel ViewModel { get; set; }
-
         public MasterView(Usuario usuario)
         {
             InitializeComponent();
-
-            this.ViewModel = new MasterViewModel(usuario);
-            this.BindingContext = ViewModel;
+            this.BindingContext = new MasterViewModel(usuario);
         }
     }
 }
