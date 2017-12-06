2a27de6e57cb4a377f8551f88c8efcc88e54978a
Gabriel Carolino Terceiro <gcarolinoterceiro@gmail.com>
2017-12-06 16:50:23 -0200

    Aula III - Editar: Adicionado botão para Enable dos EntryCells

--- TestDrive/TestDrive/ViewModels/MasterViewModel.cs
+++ TestDrive/TestDrive/ViewModels/MasterViewModel.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using TestDrive2.ViewModels;
 using Xamarin.Forms;
 
 namespace TestDrive.ViewModels
 {
-    public class MasterViewModel
+    public class MasterViewModel : BaseViewModel
     {
         #region Propriedades Públicas
         public string Nome
@@ -17,25 +18,71 @@ namespace TestDrive.ViewModels
             set { this.usuario.nome = value; }
         }
 
+        public string DataNascimento
+        {
+            get { return this.usuario.dataNascimento; }
+            set { this.usuario.dataNascimento = value; }
+        }
+
         public string Email
         {
             get { return this.usuario.email; }
             set { this.usuario.email = value; }
         }
+
+        public string Telefone
+        {
+            get { return this.usuario.telefone; }
+            set { this.usuario.telefone = value; }
+        }
+
         #endregion
 
         private readonly Usuario usuario;
 
+        private bool editando = false;
+        public bool Editando
+        {
+            get { return editando; }
+            private set
+            {
+                editando = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #region Commands
         public ICommand EditarPerfilCommand { get; private set; }
+
+        public ICommand SalvarCommand { get; private set; }
+
+        public ICommand EditarCommand { get; private set; }
+        #endregion
 
         #region Construtor
         public MasterViewModel(Usuario usuario)
         {
             this.usuario = usuario;
+            DefinirComandos();
+        }
+
+        private void DefinirComandos()
+        {
             EditarPerfilCommand = new Command(
                 () =>
                 {
                     MessagingCenter.Send<Usuario>(this.usuario, "EditarPerfil");
+                });
+            SalvarCommand = new Command(
+                () =>
+                {
+                    MessagingCenter.Send<Usuario>(this.usuario, "SucessoSalvarUsuario");
+                });
+            EditarCommand = new Command(
+                () => 
+                {
+                    this.Editando = true;
                 });
         }
         #endregion
